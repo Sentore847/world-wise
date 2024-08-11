08ffa1d63e460150e6184a6f9405bee1f1ba3ef4
Sentore847 <grigorchuk.dev@gmail.com>
2024-08-11 13:47:49 +0300

    resolve conflicts

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  // base: "",
-=======
   base: "/world-wise",
->>>>>>> b44208a545b8793b7eb5f5b2be5374ebe0c9e16c
 });
